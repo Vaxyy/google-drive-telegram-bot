0ecc9eab3d5e6dd11636cb67fbb668f53bdb7b5f
Vaxyy <91930618+Vaxyy@users.noreply.github.com>
2022-02-05 18:02:53 +0530

    update
    update

--- bot/config.py
+++ bot/config.py
@@ -65,7 +65,7 @@ class Messages:
     
     AUTH_SUCCESSFULLY = '🔐 **Authorized Google Drive account Successfully.**'
     
-    INVALID_AUTH_CODE = '❗ **Invalid Code**\n__The code you have sent is invalid or already used before. Generate new one by the Authorization URL__'
+    INVALID_AUTH_CODE = '❗ **Invalid Code**\n__The code  you have sent is invalid or already used before. Generate new one by the Authorization URL__'
     
     AUTH_TEXT = "⛓️ **To Authorize your Google Drive account visit this [URL]({}) and send the generated code here.**\n__Visit the URL > Allow permissions > you will get a code > copy it > Send it here__"
     
